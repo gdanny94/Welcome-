9d0fc2e750da4bf02dce316ddabb1400bdd45c3b
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2024-09-13 18:39:58 -0400

    Welcome example code.

---
+++ js/maini.js
@@ -0,0 +1,27 @@
+function greet(lang) {
+  
+    let dataBase = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso"
+    };
+    
+      return lang in dataBase ? dataBase[lang] : dataBase.english;
+    }
+    
+    
+    console.log(greet('spanish'))
